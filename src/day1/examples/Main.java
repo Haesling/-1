ba4f18dac8e4b75d86e5d5925621b463c22e9cd1
Anders <Andershaesling@gmail.com>
2018-09-27 11:39:27 +0200

    hej

--- src/day1/examples/Main.java
+++ src/day1/examples/Main.java
@@ -1,11 +1,12 @@
 package day1.examples;
 
+import static java.lang.Math.random;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
-
+        int Math.random()*52+1;
 
         }
 
